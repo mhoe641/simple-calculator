a311d5d3e1e6bc94e491ddea7771f439ba15d6bd
Tan Ming Hoe <tanminghoe@Tans-MacBook-Air.local>
2017-03-20 14:09:43 +0800

    Initial Commit

---
+++ Week1Assessment - Mike/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Week1Assessment - Mike
+//
+//  Created by Tan Ming Hoe on 20/03/2017.
+//  Copyright © 2017 Tan Ming Hoe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Week1Assessment - Mike/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Week1Assessment - Mike
+//
+//  Created by Tan Ming Hoe on 20/03/2017.
+//  Copyright © 2017 Tan Ming Hoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
